37c5e05e25bec67a1cb3a19fafbed480aa2d97ea
Ritanshu-01 <nijhawanritanshu@gmail.com>
2026-05-21 11:59:07 +0530

    Changed api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const envUrl = import.meta.env.VITE_API_URL;
+const baseURL = envUrl
+  ? envUrl.replace(/\/+$/, '') + (envUrl.endsWith('/api') ? '' : '/api')
+  : 'http://localhost:5000/api';
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL,
 });
 
 api.interceptors.request.use((config) => {
